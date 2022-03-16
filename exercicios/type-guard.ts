8b8ce3d2e1ca3be75a2939bbd24bbbdc7ecedd1f
Alan Jhonnes <alanjhonnes@gmail.com>
2022-03-16 18:28:57 -0300

    feat: update typescript version

--- exercicios/type-guard.ts
+++ exercicios/type-guard.ts
@@ -12,9 +12,9 @@ export interface AuthenticatedUser {
 export type User = GuestUser | AuthenticatedUser;
 
 // Essa função garante que o retorno seja um array de usuários autenticados
-export function getAuthenticatedUsers(users: User[]): AuthenticatedUser[] {
-    return users.filter(isAuthenticatedUser);
-}
+// export function getAuthenticatedUsers(users: User[]): AuthenticatedUser[] {
+//     return users.filter(isAuthenticatedUser);
+// }
 
 // Altere o retorno da função abaixo para virar um type-guard que valide que se o retorno da função for "true", o user é um LoggedInUser
 export function isAuthenticatedUser(user: User): boolean {
